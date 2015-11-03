6f4602b8f35a7c5af20f75f4a7229550c3cd2814
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2015-11-03 13:56:28 +0000

    Monkeying with the diffs.

--- app/assets/javascripts/diff.js
+++ app/assets/javascripts/diff.js
@@ -5,15 +5,13 @@ $(document).on("ready",function(e)
 
   $('.diffs').hide();
 
-  $('.column-two-thirds').prepend('<div class="diffs"> <h2 class="heading-large">'+version+' changes</h2> <p>The changes between this and the previous version of the prototype.<p> <p class="font-xsmall">Use the links on the side to view the details. <strong style="font-weight:bold">Changed</strong> files are listed first, followed by <strong style="font-weight:bold">New</strong> files, and <strong style="font-weight:bold">Deleted</strong> files. The <strong style="font-weight:bold">Ignore</strong> files are ones with small cosmetic changes which aren\'t really pertinent to the iteration of the prototype</p> <h2 class="heading-medium">Summary</h2> <dl id="summary"></dl> </div>');
+  // $('.column-two-thirds').prepend('<div class="diffs"> <h2 class="heading-large">'+version+' changes</h2> <p>The changes between this and the previous version of the prototype.<p> <p class="font-xsmall">Use the links on the side to view the details. <strong style="font-weight:bold">Changed</strong> files are listed first, followed by <strong style="font-weight:bold">New</strong> files, and <strong style="font-weight:bold">Deleted</strong> files. The <strong style="font-weight:bold">Ignore</strong> files are ones with small cosmetic changes which aren\'t really pertinent to the iteration of the prototype</p></div>');
+  $('.column-two-thirds').prepend('<div id="summary"><h2 class="heading-large">Summary</h2><dl></dl></div>');
   for(var a in data)
   {
-    $('#summary').append('<dt class="heading-small">'+a+'.html</dt>');
-    $('#summary').append('<dd>'+data[a]+'</dd>');
-  }
-
-  $(".column-third").append('<br /><h2>Ignore</h2>');
-  $(".column-third").append('<ul id="ignored"></ul>');
+    $('#summary dl').append('<dt class="heading-small">'+a+'.html</dt>');
+    $('#summary dl').append('<dd>'+data[a]+'</dd>');
+  }  
 
   $('#navs a').each(function(i,e)
   {  
@@ -30,6 +28,7 @@ $(document).on("ready",function(e)
   $('#navs a, #ignored a').on('click',function(e)
   {
     e.preventDefault();
+    $("#summary").hide();
     var id = $(this).attr('href');
     $('.diffs').hide();
     $(id).show();
