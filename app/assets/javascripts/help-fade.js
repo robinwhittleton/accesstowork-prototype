a160006902c256d556fdc7d44e7eef85945ba480
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2015-11-16 13:38:32 +0000

    changing the timer value.

--- app/assets/javascripts/help-fade.js
+++ app/assets/javascripts/help-fade.js
@@ -5,7 +5,7 @@ jQuery.fx.off = false;
 $('#help').hide();
 
 $(document).idleTimer( {
-    timeout:5000,
+    timeout:20000,
     events:'keydown',
 });
 $( document ).on( "idle.idleTimer", function(event, elem, obj){
