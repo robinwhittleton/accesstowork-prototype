9ae7f7262a78a3b3c896c17e442918689f8e03bf
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2015-11-16 13:30:58 +0000

    Putting in the fade in help in the right col.

---
+++ app/assets/javascripts/help-fade.js
@@ -0,0 +1,16 @@
+$(document).on('ready',function()
+{
+// hmm 
+jQuery.fx.off = false;    
+$('#help').hide();
+
+$(document).idleTimer( {
+    timeout:5000,
+    events:'keydown',
+});
+$( document ).on( "idle.idleTimer", function(event, elem, obj){
+    // function you want to fire when the user goes idle
+    console.log('idle!!');
+    $('#help').fadeIn(2000);
+});
+});
