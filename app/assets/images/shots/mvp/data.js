91131b2c7f38c0dddb796ffceb9210312a6dc7cf
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2015-11-19 16:59:00 +0000

    rearranging the mvp screenshots.

---
+++ app/assets/images/shots/mvp/data.js
@@ -0,0 +1,24 @@
+module.exports = {
+  webpath: "http://localhost:3001/application/mvp/",
+  outputFilename: "AtW-MVP-screens.pdf",
+  urls: [
+    'index',
+    'intro2',
+    'intro3',
+    'intro4',
+    'your-condition',
+    'help-inwork',
+    'help-travel',
+    'aboutjob',
+    'aboutwork',
+    'employed',
+    'line-manager',
+    'self-employed',
+    'limited-company',
+    'thirdparty',
+    'aboutyou',
+    'declaration',
+    'finish',
+    'email'
+  ],
+};
