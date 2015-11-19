2b2f6c17112c59b07b8eb9d6887197614a962198
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2015-11-19 16:57:36 +0000

    Doing a bit more work on the diff pages.

--- app/assets/javascripts/diff.js
+++ app/assets/javascripts/diff.js
@@ -7,10 +7,18 @@ $(document).on("ready",function(e)
 
   // $('.column-two-thirds').prepend('<div class="diffs"> <h2 class="heading-large">'+version+' changes</h2> <p>The changes between this and the previous version of the prototype.<p> <p class="font-xsmall">Use the links on the side to view the details. <strong style="font-weight:bold">Changed</strong> files are listed first, followed by <strong style="font-weight:bold">New</strong> files, and <strong style="font-weight:bold">Deleted</strong> files. The <strong style="font-weight:bold">Ignore</strong> files are ones with small cosmetic changes which aren\'t really pertinent to the iteration of the prototype</p></div>');
   $('.column-two-thirds').prepend('<div id="summary"><h2 class="heading-large">Summary</h2><dl></dl></div>');
+
+  var vprev = 'v'+(version-1);
+  var vthis = 'v'+version;
+
   for(var a in data)
   {
     $('#summary dl').append('<dt class="heading-small">'+a+'.html</dt>');
     $('#summary dl').append('<dd>'+data[a]+'</dd>');
+    $('#summary dl').append('<dd>'
+      +'<a class="screen" href="/public/images/shots/'+vprev+'/'+a+'-full.png" data-lightbox="'+a+'">'+vprev+'<br /><img src="/public/images/shots/'+vprev+'/'+a+'-full.png" width="100"></a>'
+      +'<a class="screen" href="/public/images/shots/'+vthis+'/'+a+'-full.png" data-lightbox="'+a+'">'+vthis+'<br /><img src="/public/images/shots/'+vthis+'/'+a+'-full.png" width="100"></a>'
+      +'</dd>');
   }  
 
   $('#navs a').each(function(i,e)
