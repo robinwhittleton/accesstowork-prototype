1d74a9b075ec4d4d24e006134f959a1c2b20751e
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2016-01-14 12:07:14 +0000

    Adding some faked claims data too.

---
+++ app/views/staffui/parse.js
@@ -0,0 +1,8 @@
+var csv = require('csv')
+    fs  = require('fs');
+
+var fish = fs.readFileSync('fish.csv');
+csv.parse(fish, {"columns":true}, function(err,data)
+{
+  fs.writeFileSync(__dirname + "/claims.json", JSON.stringify(data));
+});
