2261b6b976abcc34e1161470b0204b516862f2e7
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2016-01-21 17:10:54 +0000

    Mending the CSI prototype for nunjucks.

---
+++ app/views/nunjucks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# sed -i '' 's/{{<\(.*\)}}/{% extends "\1.html" %}/g' index.html
+
+FILES="*.html"
+# FILES = "index.html"
+
+sed -i '' 's/{{<[[:space:]]*\([^}]*\)[[:space:]]}}/{% extends "\1.html" %}/g' $FILES
+
+sed -i '' 's/{{<[ ]*\([a-zA-Z/]*\)[ ]*}}/{% extends "\1.html" %}/g' $FILES
+
+sed -i '' 's/{{$\([^}]*\)}}/{% block \1 %}/g' $FILES
+
+sed -i '' 's/{{\/\([^}]*\)}}/{% endblock %}/g' $FILES
+
+sed -i '' 's/{{>\([^}]*\)}}/{% include "\1.html" %}/g' $FILES
