2cef6d5d90e5ee902c975632af9f254c44953273
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2016-01-26 14:30:54 +0000

    Mending the Help column on mobile.

--- app/assets/javascripts/help-fade.js
+++ app/assets/javascripts/help-fade.js
@@ -1,16 +1,20 @@
 $(document).on('ready',function()
 {
-// hmm 
-jQuery.fx.off = false;    
-$('#help').hide();
+  // hmm
+  jQuery.fx.off = false;
 
-$(document).idleTimer( {
-    timeout:20000,
-    events:'keydown focus select change',
-});
-$( document ).on( "idle.idleTimer", function(event, elem, obj){
-    // function you want to fire when the user goes idle
-    console.log('idle!!');
-    $('#help').fadeIn(2000);
-});
+  var w = $(window).width();
+  if (w >= 640)
+  {
+    $('#help').hide();
+    $(document).idleTimer( {
+        timeout:20000,
+        events:'keydown focus select change',
+    });
+    $( document ).on( "idle.idleTimer", function(event, elem, obj){
+        // function you want to fire when the user goes idle
+        console.log('idle!!');
+        $('#help').fadeIn(2000);
+    });
+  }
 });
