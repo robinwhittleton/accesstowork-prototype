2c6a2a0a25d1733678ea2939c7fa4bf0841d97eb
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2016-02-05 14:11:40 +0000

    Some minor updates to the Feedback screen.

--- app/views/feedback/routes.js
+++ app/views/feedback/routes.js
@@ -4,14 +4,23 @@ var express     = require('express'),
     moment      = require('moment');
     router      = express.Router();
 
-router.get('/feedback/', function(req,res,next)
+router.get('/feedback/:number?', function(req,res,next)
 {
+  var number = req.params.number;
+  if (!number) number = 0;
+
   json = JSON.parse(fs.readFileSync(__dirname + "/feedback.json").toString());
   var data = _.filter(json, function(el)
   {
     return el.RATING.length > 5;
   });
-  req.data.feedback = data;
+
+  if (number >= data.length) number = 0;
+
+  req.data.feedback = data[number];
+  req.data.total = data.length;
+  req.data.number = number;
+  req.url = '/feedback/';
   next();
 });
 
