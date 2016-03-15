4ae38166d2e789d89761248045859f2e2205ae8d
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2016-03-15 12:48:35 +0000

    Adding the ability to edit a claimants status / adviser info.

--- app/views/staffui/routes.js
+++ app/views/staffui/routes.js
@@ -7,9 +7,18 @@ var express     = require('express'),
     db          = require('monk')(db_url),
     router      = express.Router();
 
+util.tog = function(data)
+{
+  var o = '<pre>';
+  o += util.inspect(data,{depth:10})
+  o += '</pre>';
+  return o;
+}
+
 router.get('/staffui/all/:order?', function(req,res,next)
 {
   var order = req.params.order;
+
   if (order == 'status') order = 'statusIndex';
   else order = 'timet';
 
@@ -21,7 +30,13 @@ router.get('/staffui/all/:order?', function(req,res,next)
       req.data = req.data || {};
       req.data.claimants  = _.sortBy(json,order);
       if (order == 'timet') req.data.claimants.reverse();
-      req.data.counts     = _.countBy(json,"status");
+
+      req.data.counts = _.countBy(json,function(item){
+        return item.status.waiting;
+      });
+
+      console.log(req.data.counts);
+
       req.url = '/staffui/all/';
       next();
     } else res.send('Empty data!');
@@ -47,10 +62,48 @@ router.get('/staffui/adviser/:id?', function(req,res,next)
   });
 });
 
+router.get('/staffui/edit/claimant/:id?/', function(req,res,next)
+{
+  var id = req.params.id;
+  if (typeof id == "undefined") res.send('Need an id in the URL please!')
+
+  var advisers = JSON.parse(fs.readFileSync(__dirname + "/data-advisers.json").toString());
+
+  var store = db.get('customers');
+  store.findById(id, function(err,data)
+  {
+    // res.send(util.tog(data));
+
+    req.data = req.data || {};
+    req.data.claimant = data;
+    req.data.advisers = advisers;
+    req.url = '/staffui/edit_claimant';
+    next();
+  });
+});
+
+router.post('/staffui/edit/claimant/', function(req,res,next)
+{
+
+    var advisers = JSON.parse(fs.readFileSync(__dirname + "/data-advisers.json").toString());
+    var adviser = _.findWhere(advisers, {"id":parseInt(req.body.adviser)});
+
+    var store = db.get('customers');
+    store.findById(req.body._id, function(err,data)
+    {
+      data.status = req.body.status;
+      data.adviser = adviser;
+      store.updateById(data._id,data, function(err,doc)
+      {
+        res.redirect('/staffui/edit/claimant/'+data._id);
+      });
+    });
+});
+
 router.get('/staffui/claimant/:id?/:page?/', function(req,res,next)
 {
   var id = req.params.id;
-  if (typeof id == "undefined") id = 0;
+  if (typeof id == "undefined") res.send('Need an id in the URL please!')
 
   var page = req.params.page;
   if (typeof page == "undefined") page = 'timeline';
@@ -75,6 +128,36 @@ router.get('/staffui/claimant/:id?/:page?/', function(req,res,next)
   // });
 
 
+});
+
+router.get('/staffui/db',function(req,res,next)
+{
+  var stati = JSON.parse(fs.readFileSync(__dirname + "/data-stati.json").toString());
+
+  var store = db.get('customers');
+  store.find({}, function(err,docs)
+  {
+    var out = '<pre>';
+    // out = util.inspect(stati,{depth:10})+"<br /><br />";
+    _.each(docs,function(el,i)
+    {
+      // var w = el.status.waiting;
+      // delete el.status.waiting;
+      // el.status['awaiting'] = w;
+
+      // output
+      out += util.inspect(el,{depth:10})+"<br /><br />";
+
+      // store.updateById(el._id,el,function(err,doc)
+      // {
+      //     // output
+      //     out += util.inspect([err,doc],{depth:10})+"\n\n";
+      // });
+
+    });
+    out += '</pre>';
+    res.send(out);
+  });
 });
 
 /*
