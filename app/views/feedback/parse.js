9b7961157449128c20ae222bfe352d46f3f5fc6b
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2016-05-09 12:13:44 +0100

    Updating the feedback page.

--- app/views/feedback/parse.js
+++ app/views/feedback/parse.js
@@ -1,8 +1,15 @@
 var csv = require('csv')
     fs  = require('fs');
 
-var fish = fs.readFileSync('feedback.csv');
-csv.parse(fish, {"columns":true}, function(err,data)
+
+if (typeof process.argv[2] != 'undefined') // make sure they've passed a file.
 {
-  fs.writeFileSync(__dirname + "/feedback.json", JSON.stringify(data));
-});
+  var file = process.argv[2];
+  var fish = fs.readFileSync(file);
+  csv.parse(fish, {"columns":true}, function(err,data)
+  {
+    fs.writeFileSync(__dirname + "/feedback.json", JSON.stringify(data));
+  });
+} else {
+  console.log("Please tell me which CSV file you want me to use.");
+}
--- app/views/feedback/routes.js
+++ app/views/feedback/routes.js
@@ -10,16 +10,34 @@ router.get('/feedback/:number?', function(req,res,next)
   if (!number) number = 0;
 
   json = JSON.parse(fs.readFileSync(__dirname + "/feedback.json").toString());
+
+  /*  removing those who don't have a
+      satisfaction rating (i.e. the string
+      isn't long enough) */
   var data = _.filter(json, function(el)
   {
     return el.RATING.length > 5;
   });
 
+  /*  If number has gone too high reset it
+      and start the loop again. */
   if (number >= data.length) number = 0;
 
+  /* Group by rating to get totals. */
+  var totals = _.groupBy(data,function(el)
+  {
+    return el.RATING.toLowerCase().trim();
+  });
+
+  /* Pass all this data to the template. */
   req.data = req.data || {};
   req.data.feedback = data[number];
-  req.data.total = data.length;
+  req.data.totals = {
+    total: data.length,
+    satisfied: Math.round(100 * totals['satisfied'].length / data.length),
+    neither: Math.round(100 * totals['neither satisfied nor dissatisfied'].length / data.length),
+    dissatisfied: Math.round(100 * totals['dissatisfied'].length / data.length)
+  };
   req.data.number = number;
   req.url = '/feedback/';
   next();
