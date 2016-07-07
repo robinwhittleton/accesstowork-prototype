a7c1c416c357df8b87e224eb2ca74b6dbdb4fb49
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2016-07-07 09:42:59 +0100

    Continued work setting up an MVP version of the staff ui.

--- app/views/staffui/mvp/routes.js
+++ app/views/staffui/mvp/routes.js
@@ -15,19 +15,56 @@ router.get('/staffui/mvp/', function(req,res,next)
 {
   req.data = req.data || {};
   req.data.cases = customers;
-  // var keys = [];
-  // for (var i = 0; i < customers.length; i++) {
-  //   for (key in customers[i])
-  //   {
-  //     keys.push(key);
-  //   }
-  // }
-  // var u = _.uniq(keys);
-  // console.log(u); 
   next();  
 });
 
-router.get('/staffui/mvp/:id', function(req,res,next)
+router.get('/staffui/mvp/groupby/all', function(req,res,next)
+{
+  res.redirect('/staffui/mvp/');
+  next();  
+});
+
+router.get('/staffui/mvp/advisers/', function(req,res,next)
+{
+  var customers = JSON.parse(fs.readFileSync(__dirname + "/new_customers.json").toString());  
+  var advisers  = _.uniq(_.pluck(customers,"adviser"),false,function(p){ return p.id });
+  
+  req.data = req.data || {};
+  req.data.advisers = advisers;
+  next();  
+});
+
+router.get('/staffui/mvp/adviser/:id', function(req,res,next)
+{
+  var id = parseInt(req.params.id);
+  
+  var cases = _.filter(customers, function(el)
+  {
+    console.log(el.adviser.id,id); 
+    return el.adviser.id == id;
+  });
+
+  req.data = req.data || {};
+  req.data.cases = cases;
+  req.url = '/staffui/mvp/adviser/';
+  next();  
+});
+
+router.get('/staffui/mvp/groupby/:by', function(req,res,next)
+{
+  var by = req.params.by;
+  
+  var cases = _.groupBy(customers,'allocation');
+  console.log(cases); 
+  
+  req.data = req.data || {};
+  req.data.by = by;
+  req.data.cases = cases[by];
+  req.url = '/staffui/mvp/';
+  next();  
+});
+
+router.get('/staffui/mvp/customer/:id', function(req,res,next)
 {
   var id = req.params.id;
   
