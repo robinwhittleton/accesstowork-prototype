063bf6434f3f27bcf2af9c880efa8284f1bb1f8c
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2016-07-11 15:16:04 +0100

    Updating the database (monk) package.

--- app/views/staffui/mvp/routes.js
+++ app/views/staffui/mvp/routes.js
@@ -24,10 +24,31 @@ router.get('/staffui/mvp/login', function(req,res,next)
 
 router.get('/staffui/mvp/', function(req,res,next)
 {
-  store.find().then(function(cases)
+  store.find({"open":true}).then(function(cases)
   {
     req.data = req.data || {};
     req.data.cases = cases;  
+    next();  
+  });
+});
+
+/*
+  ALL CASES GROUPS BY CATEGORY
+*/
+router.get('/staffui/mvp/groupby/:by', function(req,res,next)
+{
+  var by = req.params.by;
+  
+  if (by == "all") res.redirect('/staffui/mvp/');
+  
+  store.find({"open":true}).then(function(cases)
+  {
+    var cases = _.groupBy(cases,'allocation');
+    
+    req.data = req.data || {};
+    req.data.by = by;
+    req.data.cases = cases[by];
+    req.url = '/staffui/mvp/';
     next();  
   });
 });
@@ -37,13 +58,10 @@ router.get('/staffui/mvp/', function(req,res,next)
 */
 router.get('/staffui/mvp/advisers/', function(req,res,next)
 {
-  store.find().then(function(cases)
-  {
-    var advisers  = _.uniq(_.pluck(cases,"adviser"),false,function(p){ return p.id });    
-    req.data = req.data || {};
-    req.data.advisers = advisers;
-    next();  
-  });
+  var advisers = JSON.parse(fs.readFileSync(__dirname + "/_data/advisers.json").toString());
+  req.data = req.data || {};
+  req.data.advisers = advisers;
+  next();
 });
 
 /*
@@ -53,7 +71,7 @@ router.get('/staffui/mvp/adviser/:id', function(req,res,next)
 {
   var id = parseInt(req.params.id);
   
-  store.find().then(function(cases)
+  store.find({"open":true}).then(function(cases)
   {
     cases = _.filter(cases,function(el)
     {
@@ -67,36 +85,18 @@ router.get('/staffui/mvp/adviser/:id', function(req,res,next)
 });
 
 /*
-  ALL CASES GROUPS BY CATEGORY
+  UPDATE - THIS CASE'S ADVISER
 */
-router.get('/staffui/mvp/groupby/:by', function(req,res,next)
-{
-  var by = req.params.by;
-  
-  if (by == "all") res.redirect('/staffui/mvp/');
-  
-  store.find().then(function(cases)
-  {
-    var cases = _.groupBy(cases,'allocation');
-    
-    req.data = req.data || {};
-    req.data.by = by;
-    req.data.cases = cases[by];
-    req.url = '/staffui/mvp/';
-    next();  
-  });
-});
-
 router.post('/staffui/mvp/customer/adviser/update', function(req,res,next)
 {  
   var cid = req.body.case_id;
   var aid = parseInt(req.body.adviser_id);
   var advisers = JSON.parse(fs.readFileSync(__dirname + "/_data/advisers.json").toString());
   var newad = _.findWhere(advisers,{"id":aid});  
-  store.findById(cid,function(err,the_case)
+  store.findOne({"_id":cid},function(err,the_case)
   {
     the_case.adviser = newad;
-    store.updateById(the_case._id,the_case,function(err,doc)
+    store.update({"_id":the_case._id},the_case,function(err,doc)
     {
       res.status(200);
       res.send(JSON.stringify(newad));
@@ -104,31 +104,18 @@ router.post('/staffui/mvp/customer/adviser/update', function(req,res,next)
   });
 });
 
-router.post('/staffui/mvp/customer/cat/update', function(req,res,next)
-{  
-  var cid = req.body.case_id;
-  var cat = req.body.category;
-
-  store.findById(cid,function(err,the_case)
-  {
-    the_case.allocation = cat;
-    store.updateById(the_case._id,the_case,function(err,doc)
-    {
-      res.status(200);
-      res.send("success");
-    });
-  });
-});
-
+/*
+  UPDATE - THIS CASE'S IN DISC MARKER ("open")
+*/
 router.post('/staffui/mvp/customer/disc/update',function(req,res,next)
 {
   var cid = req.body.case_id;
   var open = req.body.open;
   
-  store.findById(cid,function(err,the_case)
+  store.findOne({"_id":cid},function(err,the_case)
   {
     the_case.open = open;
-    store.updateById(the_case._id,the_case,function(err,doc)
+    store.update({"_id":the_case._id},the_case,function(err,doc)
     {
       res.status(200);
       res.send(JSON.stringify(doc));
@@ -136,15 +123,18 @@ router.post('/staffui/mvp/customer/disc/update',function(req,res,next)
   });
 });
 
+/*
+  UPDATE - THIS CASE'S CATEGORY
+*/
 router.post('/staffui/mvp/customer/cat/update', function(req,res,next)
 {  
   var cid = req.body.case_id;
   var cat = req.body.category;
 
-  store.findById(cid,function(err,the_case)
+  store.findOne({"_id":cid},function(err,the_case)
   {
     the_case.allocation = cat;
-    store.updateById(the_case._id,the_case,function(err,doc)
+    store.update({"_id":the_case._id},the_case,function(err,doc)
     {
       res.status(200);
       res.send("success");
@@ -160,7 +150,7 @@ router.get('/staffui/mvp/customer/:id/:what?', function(req,res,next)
   var id = req.params.id;
   var what = (req.params.what) ? '_'+req.params.what : '_application';
   
-  store.findById(id,function(err,cases)
+  store.findOne({"_id":id},function(err,cases)
   {
     var advisers = JSON.parse(fs.readFileSync(__dirname + "/_data/advisers.json").toString());  
     
