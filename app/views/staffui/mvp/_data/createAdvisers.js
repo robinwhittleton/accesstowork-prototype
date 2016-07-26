a7697ec913c379283ca7a511d704436864502780
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2016-07-26 13:46:09 +0100

    Adding teams to the adviser data.

--- app/views/staffui/mvp/_data/createAdvisers.js
+++ app/views/staffui/mvp/_data/createAdvisers.js
@@ -4,7 +4,8 @@ var fs          = require('fs'),
 var ads = JSON.parse(fs.readFileSync(__dirname + "/advisers.json").toString());
 
 for (var i = 0; i < ads.length; i++) {
-  ads[i].id = i;
+  // ads[i].id = i;
+  ads[i].team = _.sample(['deaf','hidden','large','self','director','visual','pan']);
 }
 
 fs.writeFileSync(__dirname + "/advisers.json", JSON.stringify(ads));
